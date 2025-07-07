857268cfea1596dc6710a151c0635ae66887653f
eccho03 <choeunchae0828@gmail.com>
2025-07-07 12:04:10 +0900

    httpserver proxy + wifi direct:

--- app/src/main/java/com/example/wifi_direct_proxy/MainActivity.kt
+++ app/src/main/java/com/example/wifi_direct_proxy/MainActivity.kt
@@ -11,6 +11,7 @@ import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
+import com.example.wifi_direct_proxy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,6 +34,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnStart.setOnClickListener {
+            val port = binding.etPort.text.toString().toIntOrNull() ?: 1081
+            startService(HttpProxyService.newStartIntent(this, port))
+            binding.etPort.setText(port.toString())
+        }
+        binding.btnStop.setOnClickListener {
+            stopService(HttpProxyService.newStopIntent(this))
+        }
+        useSystemProxy = binding.cbUseSystemProxy.isChecked
+        binding.cbUseSystemProxy.setOnCheckedChangeListener { _, isChecked ->
+            useSystemProxy = isChecked
+        }
 
         manager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
         channel = manager.initialize(this, mainLooper, null)
@@ -147,6 +163,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+        stopService(HttpProxyService.newStopIntent(this))
         unregisterReceiver(wifiDirectReceiver)
     }
 }
